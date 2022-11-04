ba55d2e112001add84fa4365fbd9c22b379e60bb
timtiph <tiphany.moine@outlook.com>
2022-11-04 13:07:54 +0100

    counterReducer

--- src/index.js
+++ src/index.js
@@ -2,13 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { createStore } from 'redux'
+import { Provider } from "react-redux";
 
+const Store = createStore(CounterReducer);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={Store}>
+
+  <App />
+
+</Provider>
+
 );
 
 
