1bf95d6f742474a64cc7acd7a2854ff11655f1f7
timtiph <tiphany.moine@outlook.com>
2022-11-04 13:46:33 +0100

    incr decr du state

--- src/Reducers/CompterReducer.js
+++ src/Reducers/CompterReducer.js
@@ -4,6 +4,21 @@ const INITIAL_STATE = {
 
 function CounterReducer(state = INITIAL_STATE, action){
 
+    switch(action.type){
+        case 'INCR': {
+            return {
+                ...state,
+                count: state.count + 1
+            }
+        }
+        case 'DECR': {
+            return {
+                ...state,
+                count: state.count - 1
+            }
+        }
+    }
+
 
     return state;
 }
--- src/index.js
+++ src/index.js
@@ -2,19 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { createStore } from 'redux'
+import { createStore } from "redux";
 import { Provider } from "react-redux";
+import CounterReducer from "./Reducers/CompterReducer";
 
 const Store = createStore(CounterReducer);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={Store}>
-
-  <App />
-
-</Provider>
-
+    <App />
+  </Provider>,
 );
-
-
